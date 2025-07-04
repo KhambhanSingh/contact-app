2c1098c6abe114914b04d297ab9489237fad4887
KhambhanSingh <deshmukhkhambhan@gmail.com>
2025-07-04 16:00:53 +0530

    contact-app-MVC-errorHandling

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,49 +1,27 @@
 import express from 'express';
-import Contact from "../models/contacts.model.js";
 const router = express.Router();
 
-router.get('/',async (req, res) => {
-    const contacts = await Contact.find();
-    res.render('home', {contacts: contacts});
-});
+import { Contacts,
+    contactPage,
+    addContact,
+    addContactPage,
+    updateContactPage,
+    updateContact,
+    deleteContact
+ } from "../controller/contact.controller.js";
 
-router.get('/show-contact/:id', async (req, res) => {
-    // const contact_detail = await Contact.findOne({"_id": req.params.id})
-    const contact_detail = await Contact.findById(req.params.id)
-    res.render('show-contact', {contact_detail: contact_detail});
-});
+router.get('/', Contacts);
 
-router.get('/add-contact', (req, res) => {
-    res.render('add-contact');
-});
+router.get('/show-contact/:id', contactPage);
 
-router.post('/add-contact', async (req, res) => {
-    // const contact_detail = await Contact.insertOne({
-    //     first_name: req.body.first_name,
-    //     last_name: req.body.last_name,
-    //     email: req.body.email,
-    //     phone: req.body.phone,
-    //     address: req.body.address
-    // });
-    await Contact.create(req.body)
-    res.redirect('/');
-});
+router.get('/add-contact', addContactPage);
 
-router.get('/update-contact/:id', async (req, res) => {
-    const contact_detail = await Contact.findById(req.params.id)
-    res.render('update-contact', {contact_detail: contact_detail});
-});
+router.post('/add-contact', addContact);
 
-router.post('/update-contact/:id', async (req, res) => {
-    // const {first_name, last_name, email, phone, address} = req.body;
-    // await Contact.findByIdAndUpdate(req.params.id,{first_name, last_name, email, phone, address})
-    await Contact.findByIdAndUpdate(req.params.id,req.body)
-    res.redirect('/');
-});
+router.get('/update-contact/:id', updateContactPage);
 
-router.get('/delete-contact/:id', async (req, res) => {
-    await Contact.findByIdAndDelete(req.params.id)
-    res.redirect('/');
-});
+router.post('/update-contact/:id', updateContact);
+
+router.get('/delete-contact/:id', deleteContact);
 
 export default router;
